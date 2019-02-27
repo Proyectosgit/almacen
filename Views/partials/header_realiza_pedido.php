0fa33033bd18a77c32ab7068197f5c86b2d50545
aybalmacen <ivancmf789@gmail.com>
2019-02-27 16:20:25 -0600

    Se agregan iconos a cosina

--- Views/partials/header_realiza_pedido.php
+++ Views/partials/header_realiza_pedido.php
@@ -4,10 +4,23 @@
         <a class="navbar-brand" href="#">A&B</a>
     </div>
     <ul class="nav">
-        <li class="active"><a href="?controller=producto&action=search_prod">Realizar pedido</a></li>
-        <li class="active"><a href="?controller=pedido&action=ver_pedido_autorizado">Pedidos autorizados</a></li>
-        <li class="active"><a href="?controller=pedido&action=ver_pedido_cancelado">Pedidos cancelados</a></li>
-        <li><a href="Views/sesion/cerrar_sesion.php">Cerrar sesion</a></li>
+        <li class="active"><a href="?controller=producto&action=search_prod"><span class="oi" data-glyph="cart"></span>
+          Realizar pedido
+          </a>
+        </li>
+        <li class="active"><a href="?controller=pedido&action=ver_pedido_autorizado"><span class="oi" data-glyph="check"></span>
+          Pedidos autorizados
+          </a>
+        </li>
+        <li class="active">
+          <a href="?controller=pedido&action=ver_pedido_cancelado"><span class="oi" data-glyph="x"></span>
+            Pedidos cancelados
+          </a>
+        </li>
+        <li><a href="Views/sesion/cerrar_sesion.php"><span class="oi" data-glyph="account-logout"></span>
+          Cerrar sesion
+          </a>
+        </li>
     </ul>
   </div>
 </nav>
