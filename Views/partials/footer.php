7bd3d0ed71d1bf35a8b95def084c5ccb33fe4f67
aybalmacen <ivancmf789@gmail.com>
2019-03-06 11:06:58 -0600

    Se agrega logo a gerente y se agrega correo de gmail de contacto

--- Views/partials/footer.php
+++ Views/partials/footer.php
@@ -2,5 +2,5 @@
     <div class="footer-copyright text-center py-3">© 2019 Copyright:
         <a href="http://piacevoleristorante.com/"> Piacevole</a>
     </div>
-    <H5 align="center">Contacto: <a href="mailto:sistemas@example.com">sistemas@example.com</a>.</H5>
+    <H5 align="center">Contacto: <a href="mailto:ayb.soportecorporativo@gmail.com">ayb.soportecorporativo@gmail.com</a></H5>
 </footer>
