d9fc788176289155d15a8c3a61b12f00ce1351b1
aybalmacen <ivancmf789@gmail.com>
2019-04-10 09:16:56 -0500

    cambios

--- 3ra_ronda_angelopolis/Controllers/actualiza_controller.php
+++ 3ra_ronda_angelopolis/Controllers/actualiza_controller.php
@@ -5,16 +5,19 @@
 
         public function get_info_archivo(){
             $ruta = "C:\PUE\\3ajuarez";
-            $nombre_archivo = "OCOMPRA.CSV";
+            $nombre_archivo = "OCOMPRA1.CSV";
             require_once("Config/config.php");
             require_once("Models/actualiza.php");
             $ObjActualiza = Actualiza::get_info_archivo($ruta,$nombre_archivo);
-            Actualiza::save($ObjActualiza);
-            // print_r($ObjActualiza);
-            // print_r($ObjActualiza->nombre_archivo);
-            $ObjActualiza = Actualiza::check_actualizacion($ObjActualiza->nombre_archivo, $ObjActualiza->hora, $ObjActualiza->fecha, $ObjActualiza->peso);
-            print_r($ObjActualiza);
-
+            if(isset($ObjActualiza)){
+                Actualiza::save($ObjActualiza);
+                // print_r($ObjActualiza);
+                // print_r($ObjActualiza->nombre_archivo);
+                $ObjActualiza = Actualiza::check_actualizacion($ObjActualiza->nombre_archivo, $ObjActualiza->hora, $ObjActualiza->fecha, $ObjActualiza->peso);
+                print_r($ObjActualiza);
+            }else{
+                echo "No esta definida";
+            }
         }
 
     }
