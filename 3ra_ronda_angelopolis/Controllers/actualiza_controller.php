b2d77d41cfc3f89c32fd80f698ea19f732161679
aybalmacen <ivancmf789@gmail.com>
2019-04-10 19:20:23 -0500

    cambios en controller actualiza

--- 3ra_ronda_angelopolis/Controllers/actualiza_controller.php
+++ 3ra_ronda_angelopolis/Controllers/actualiza_controller.php
@@ -4,20 +4,6 @@
         public function __construct(){}
 
         public function get_info_archivo(){
-<<<<<<< HEAD
-            $ruta_and_csv_ocompra = "C:\PUE\\3ajuarez\OCOMPRA.CSV";
-            require_once("Config/config.php");
-            require_once("Models/actualiza.php");
-            $ObjActualiza = Actualiza::get_info_archivo($ruta_and_csv_ocompra);
-
-            $existe = Actualiza::check_actualizacion($ObjActualiza->nombre_archivo, $ObjActualiza->hora, $ObjActualiza->fecha, $ObjActualiza->peso);
-            if($existe == true){
-                // echo "Ya existe la actualizacion";
-                return false;
-            }else{
-                $inserta = Actualiza::save($ObjActualiza);
-                return true;
-=======
             $ruta = "C:\PUE\\3ajuarez";
             $nombre_archivo = "OCOMPRA1.CSV";
             require_once("Config/config.php");
@@ -32,7 +18,6 @@
             }else{
                 echo "No esta definida";
 
->>>>>>> aa82b852dbe79133f4fb94711a611074196cdea2
             }
         }
 
